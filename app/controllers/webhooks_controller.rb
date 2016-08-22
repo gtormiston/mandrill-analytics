fbafcf0099e40f0d5d1d93b6a63da80dd3db3c60
gtormiston <grahamormiston@gmail.com>
2016-08-22 02:54:28 +0100

    testing JSON POST request from llirdnam works

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,9 +1,15 @@
 class WebhooksController < ApplicationController
-
+  
   def index
   end
 
-  def update
+  # POST /webhooks.json
+  def create
+    information = request.raw_post
+    data_parsed = JSON.parse(information)
+    p data_parsed
+
+    render json: data_parsed
   end
 
 end
